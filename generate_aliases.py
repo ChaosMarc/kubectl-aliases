12a76dae72e6f87d426b20abf22608a9a185adde
Marc Weiß <marc.weiss@prodyna.com>
2022-09-28 15:13:41 +0200

    Shorten oyaml, owide and ojson to oy, ow, and oj

--- generate_aliases.py
+++ generate_aliases.py
@@ -63,13 +63,13 @@ def main():
     res_types = [r[0] for r in res]
 
     args = [
-        ('oyaml', '-o=yaml', ['g'], ['owide', 'ojson', 'sl']),
-        ('owide', '-o=wide', ['g'], ['oyaml', 'ojson']),
-        ('ojson', '-o=json', ['g'], ['owide', 'oyaml', 'sl']),
+        ('oy', '-o=yaml', ['g'], ['ow', 'oj', 'sl']),
+        ('ow', '-o=wide', ['g'], ['oy', 'oj']),
+        ('oj', '-o=json', ['g'], ['ow', 'oy', 'sl']),
         ('all', '--all-namespaces', ['g', 'd'], ['rm', 'f', 'no', 'sys']),
-        ('sl', '--show-labels', ['g'], ['oyaml', 'ojson'], None),
+        ('sl', '--show-labels', ['g'], ['oy', 'oj'], None),
         ('all', '--all', ['rm'], None), # caution: reusing the alias
-        ('w', '--watch', ['g'], ['oyaml', 'ojson', 'owide']),
+        ('w', '--watch', ['g'], ['oy', 'oj', 'ow']),
         ]
 
     # these accept a value, so they need to be at the end and
